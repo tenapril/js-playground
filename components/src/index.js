72d658f0aad2c16f509e780fcd764ff5f6761066
Andrew T <andrewtirtokusumo@gmail.com>
2020-02-21 19:07:44 +0700

    playground chapter 3 finished

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        comment={faker.lorem.sentences()}
-        avatarImage={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00PM"
-        comment={faker.lorem.sentences()}
-        avatarImage={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        comment={faker.lorem.sentences()}
-        avatarImage={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>WARNING!</h4>
+          Are you sure?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="now"
+          comment={faker.lorem.sentences()}
+          avatarImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          comment={faker.lorem.sentences()}
+          avatarImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          comment={faker.lorem.sentences()}
+          avatarImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
